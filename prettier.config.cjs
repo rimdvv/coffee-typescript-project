278c6e7526027a42f4456031b328e9f430ac0231
Davin <davinrim10@gmail.com>
2023-08-09 23:39:08 +0900

    Navbar

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+// module.exports = {
+//   plugins: [require("prettier-plugin-tailwindcss")],
+// };
+
+module.exports = {
+
+  plugins: [require("prettier-plugin-tailwindcss")],
+  
+  trailingComma: "es5",
+  
+  arrowParens: "avoid",
+  
+  };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Navbar from '@/scenes/navbar'; //폴더안에 index.tsx있는 경우, 그 폴더까지만 경로 적어줘도 됨. index.tsx만
+import { useState } from 'react';
+
 function App() {
-  return <></>;
+  const [selectedPage, setSelectedPage] = useState<string>('home');
+  return (
+    <div className="bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
