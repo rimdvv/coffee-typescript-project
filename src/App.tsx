0b60d88c7897c4687dc5265df33d5fd42bd9cb20
Davin <davinrim10@gmail.com>
2023-08-10 16:59:29 +0900

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import Navbar from '@/scenes/navbar'; //폴더안에 index.tsx있는 경우, 그 폴더까지만 경로 적어줘도 됨. index.tsx만
-import { useState } from 'react';
+import Home from './scenes/home';
+import { useEffect, useState } from 'react';
+import { SelectedPage } from '@/shared/types';
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>('home');
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll); //to prevent memory leak or bugs
+  }, []);
+
   return (
-    <div className="bg-gray-20">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    <div className="app bg-gray-20">
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+        isTopOfPage={isTopOfPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
